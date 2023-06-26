0f5f578a1d3b926391f2dd6f4097f86aabaec886
SrMoffat <ngigemoffat@gmail.com>
2023-06-26 20:59:11 +0300

    feat(modal): add create item modal

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,5 +1,108 @@
+import React from 'react';
+import type { MenuProps } from 'antd';
+import {
+    AppstoreOutlined,
+    BarChartOutlined,
+    CloudOutlined,
+    ShopOutlined,
+    TeamOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Divider, Steps, Layout, Menu, theme } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+
+const items: MenuProps['items'] = [
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    BarChartOutlined,
+    CloudOutlined,
+    AppstoreOutlined,
+    TeamOutlined,
+    ShopOutlined,
+].map((icon, index) => ({
+    key: String(index + 1),
+    icon: React.createElement(icon),
+    label: `nav ${index + 1}`,
+}));
+
 const Product = () => {
-    return 'Single Product'
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+    return (
+        <Layout hasSider>
+            <Sider
+                style={{
+                    overflow: 'auto',
+                    height: '100vh',
+                    position: 'fixed',
+                    left: 0,
+                    top: 0,
+                    bottom: 0,
+                }}
+            >
+                <div className="demo-logo-vertical" />
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+            </Sider>
+            <Layout className="site-layout" style={{ marginLeft: 200 }}>
+                <Header style={{ padding: 0, background: colorBgContainer }} />
+                <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                    <Steps
+                        progressDot
+                        current={1}
+                        items={[
+                            {
+                                title: 'Finished',
+                                description: 'This is a description.',
+                            },
+                            {
+                                title: 'In Progress',
+                                description: 'This is a description.',
+                            },
+                            {
+                                title: 'Waiting',
+                                description: 'This is a description.',
+                            },
+                        ]}
+                    />
+                    <Divider />
+                    <Steps
+                        progressDot
+                        current={1}
+                        direction="vertical"
+                        items={[
+                            {
+                                title: 'Finished',
+                                description: 'This is a description. This is a description.',
+                            },
+                            {
+                                title: 'Finished',
+                                description: 'This is a description. This is a description.',
+                            },
+                            {
+                                title: 'In Progress',
+                                description: 'This is a description. This is a description.',
+                            },
+                            {
+                                title: 'Waiting',
+                                description: 'This is a description.',
+                            },
+                            {
+                                title: 'Waiting',
+                                description: 'This is a description.',
+                            },
+                        ]}
+                    />
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+            </Layout>
+        </Layout>
+    );
 };
 
 export default Product;
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     AppstoreOutlined,
     BarChartOutlined,
@@ -10,7 +10,7 @@ import {
     VideoCameraOutlined,
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { Layout, Menu, theme } from 'antd';
+import { Layout, Menu, theme, Button, Modal } from 'antd';
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -30,12 +30,42 @@ const items: MenuProps['items'] = [
 }));
 
 const Products = () => {
+    const [open, setOpen] = useState(false);
+    const [modalText, setModalText] = useState('Content of the modal');
+    const [confirmLoading, setConfirmLoading] = useState(false);
     const {
         token: { colorBgContainer },
     } = theme.useToken();
+    const handleShowCreateItemModal = () => {
+        setOpen(true);
+    };
+    const handleOk = () => {
+        setModalText('The modal will be closed after two seconds');
+        setConfirmLoading(true);
+        setTimeout(() => {
+            setOpen(false);
+            setConfirmLoading(false);
+        }, 2000);
+    };
+
+    const handleCancel = () => {
+        console.log('Clicked cancel button');
+        setOpen(false);
+    };
 
     return (
         <Layout hasSider>
+            {open && (
+                <Modal
+                    title="Title"
+                    open={open}
+                    onOk={handleOk}
+                    confirmLoading={confirmLoading}
+                    onCancel={handleCancel}
+                >
+                    <p>{modalText}</p>
+                </Modal>
+            )}
             <Sider
                 style={{
                     overflow: 'auto',
@@ -50,7 +80,11 @@ const Products = () => {
                 <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
             </Sider>
             <Layout className="site-layout" style={{ marginLeft: 200 }}>
-                <Header style={{ padding: 0, background: colorBgContainer }} />
+                <Header style={{ padding: 0, background: colorBgContainer }}>
+                    <Button size="large" type="primary" shape="round" onClick={handleShowCreateItemModal}>
+                        Create Item
+                    </Button>
+                </Header>
                 <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
                     <div style={{ padding: 24, textAlign: 'center', background: colorBgContainer }}>
                         <p>long content</p>
