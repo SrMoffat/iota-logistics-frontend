8fac9a3228ef131e2c723269a5f215caab7d9d06
SrMoffat <ngigemoffat@gmail.com>
2023-06-27 07:49:43 +0300

    chore(fetching): add dev tools for react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,36 @@
-import '../styles/global.css'
-import { AppProps } from 'next/app'
+import { useState, useEffect, Suspense, lazy } from 'react';
+import { AppProps } from 'next/app';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import '../styles/global.css';
+
+const queryClient = new QueryClient();
+
+const ReactQueryDevtoolsProduction = lazy(() =>
+  import('@tanstack/react-query-devtools/build/lib/index.prod.js').then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    }),
+  ),
+)
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [showDevtools, setShowDevtools] = useState(false)
+
+  useEffect(() => {
+    // @ts-ignore
+    window.toggleDevtools = () => setShowDevtools((old) => !old)
+  }, [])
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen />
+      {showDevtools && (
+        <Suspense fallback={null}>
+          <ReactQueryDevtoolsProduction />
+        </Suspense>
+      )}
+    </QueryClientProvider>
+  )
 }
